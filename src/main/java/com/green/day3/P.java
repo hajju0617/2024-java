2f458698b6a9788de331bff0493e71a4e6f6ffa8
hajju0617 <joowon023617@gmail.com>
2024-03-18 18:06:52 +0900

    first

--- src/main/java/com/green/day3/P.java
+++ src/main/java/com/green/day3/P.java
@@ -2,7 +2,7 @@ package com.green.day3;
 
 public class P {
     public static void main(String[] args) {
-        int i,j=0;
+        int i,j;
 
         for(i=0; i<5; i++) {
             for(j=0; j<5; j++) {
