0bbaba1325c955590806c723322dcc85fd1ed7e9
hajju0617 <joowon023617@gmail.com>
2024-03-21 16:10:11 +0900

    first

---
+++ src/main/java/com/green/day6/ch3/AssignmentOperator.java
@@ -0,0 +1,12 @@
+package com.green.day6.ch3;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int x, y;
+        // left value( 저장할 수 있는 공간 ex.변수,배열...)
+        // right value ( 연산식, 변수, 리터럴)
+        x = y = 3;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
