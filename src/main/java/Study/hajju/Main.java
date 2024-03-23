b785624b1fd4d3fdf403be40394daa41d45f95a6
hajju0617 <joowon023617@gmail.com>
2024-03-23 23:20:10 +0900

    first

---
+++ src/main/java/Study/hajju/Main.java
@@ -0,0 +1,4 @@
+package Study.hajju;
+
+public class Main {
+}
---
+++ src/main/java/Study/hajju/ch4/FlowEx8.java
@@ -0,0 +1,19 @@
+package Study.hajju.ch4;
+import java.util.*;
+public class FlowEx8 {
+    public static void main(String[] args) {
+        System.out.print("당신의 주민등록번호를 입력하세요. (******-*******) :");
+
+        Scanner scan = new Scanner(System.in);
+        String regNo = scan.nextLine();
+        String result;
+
+        char gender = regNo.charAt(7);
+        switch (gender) {
+            case '1','3' -> result = "남자";
+            case '2','4' -> result = "여자";
+            default -> result = result = "알 수 없는 사람";
+        }
+        System.out.printf("당신은 %s입니다", result);
+    }
+}
