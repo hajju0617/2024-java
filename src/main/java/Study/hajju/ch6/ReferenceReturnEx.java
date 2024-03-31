2c60a9ebac88762f32cb1dd5ec6eaf608d763d99
hajju0617 <joowon023617@gmail.com>
2024-03-31 16:13:05 +0900

    first

---
+++ src/main/java/Study/hajju/ch6/ReferenceReturnEx.java
@@ -0,0 +1,21 @@
+package Study.hajju.ch6;
+class Data3 { int x; }
+
+public class ReferenceReturnEx {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.x = 10;
+
+        Data d2 = copy(d);
+        System.out.println("d.x = " + d.x);
+        System.out.println("d2.x = " + d2.x);
+
+    }   //main
+
+    static Data copy(Data d) {
+        Data tmp = new Data();
+        tmp.x = d.x;
+
+        return tmp;
+    }
+}
