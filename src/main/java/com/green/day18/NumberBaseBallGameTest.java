30abdc20a0ef3539ee60d1dc8e89b710cc7f374c
hajju0617 <joowon023617@gmail.com>
2024-04-08 17:06:57 +0900

    first

--- src/main/java/com/green/day18/NumberBaseBallGameTest.java
+++ src/main/java/com/green/day18/NumberBaseBallGameTest.java
@@ -1,8 +1,12 @@
 package com.green.day18;
 
+import java.util.Arrays;
+
 public class NumberBaseBallGameTest {
     public static void main(String[] args) {
         NumberBaseBallGame game = new NumberBaseBallGame(3);
+        game.showArr();
+        //game.start();
     }
 }
 
@@ -13,32 +17,32 @@ class NumberBaseBallGame {
     final int NUMBER_COUNT;
     private int[] numArr;
     NumberBaseBallGame(final int NUMBER_COUNT) {
-        if(NUMBER_COUNT < 1 || NUMBER_COUNT > 9) {
+        if(NUMBER_COUNT < 1 || NUMBER_COUNT > MAX_NUMBER) {
             this.NUMBER_COUNT = DEFAULT_NUMBER;
         } else {
             this.NUMBER_COUNT = NUMBER_COUNT;
         }
-        this.NUMBER_COUNT = NUMBER_COUNT;
         this.init();
     }
     private void init() {
         this.numArr = new int[NUMBER_COUNT];
-
+        Main:
         for(int i = 0; i < this.numArr.length; i++) {
-            int val = (int)((Math.random() * 9) + 1);
-            numArr[i] = val;
+            int val = getRandomValue();
             for(int j = 0; j < i; j++) {
-                if(numArr[j] == val) {
+                if(this.numArr[j] == val) {
                     i--;
-                    break;
+                    continue Main;
                 }
             }
+            this.numArr[i] = val;
         }
     }   //init
     private int getRandomValue() {
         return (int)((Math.random() * MAX_NUMBER) + MIN_NUMBER);
     }
     public void showArr() {
+        System.out.println(Arrays.toString(this.numArr));
 
     }
 }
