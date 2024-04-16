0f11e211a1c588c14c0d6a0e66a1e9d330ebb41f
hajju0617 <joowon023617@gmail.com>
2024-04-17 02:48:38 +0900

    first

--- src/main/java/Study/hajju/ch6/MemberCall.java
+++ src/main/java/Study/hajju/ch6/MemberCall.java
@@ -7,13 +7,26 @@ public class MemberCall {
     int iv2 = cv;
 //    static int cv2 = iv;      // 클래스 변수는 인스턴스 변수를 사용할 수 X
     static int cv2 = new MemberCall().iv;
-    MemberCall mb = new MemberCall();
-//    static int cv3 = mb.iv;
-//    static int cv4 = iv;
+
 
     static void staticMethod1() {
         System.out.println(cv);
 //        System.out.println(iv);
-
+        MemberCall c = new MemberCall();
+        System.out.println(c.iv);
+    }
+    void instanceMethod1() {
+        System.out.println(cv);
+        System.out.println(iv);
+    }
+    static void staticMethod() {
+        staticMethod1();
+//        instanceMethod1();
+        MemberCall c = new MemberCall();
+        c.instanceMethod1();
+    }
+    void instanceMethod2(){
+        staticMethod1();
+        instanceMethod1();
     }
 }
