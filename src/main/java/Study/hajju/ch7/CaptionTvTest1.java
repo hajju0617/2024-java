658ccb6cbf9a2d023825bc9ef1db01ae593d465b
hajju0617 <joowon023617@gmail.com>
2024-04-21 15:45:03 +0900

    first

--- src/main/java/Study/hajju/ch7/CaptionTvTest1.java
+++ src/main/java/Study/hajju/ch7/CaptionTvTest1.java
@@ -1,24 +1,24 @@
 package Study.hajju.ch7;
 
 public class CaptionTvTest1 {
-    public static void main(String[] args) {
-        CaptionTv ctv = new CaptionTv();
-        ctv.channel = 10;
-        ctv.channelUp();
-        System.out.println("ctv.channel : " + ctv.channel);
-        ctv.displayCation("Hello, World");
-        ctv.caption = true;
-        ctv.displayCation("Hello, World");
-    }
+//    public static void main(String[] args) {
+//        CaptionTv ctv = new CaptionTv();
+//        ctv.channel = 10;
+//        ctv.channelUp();
+//        System.out.println("ctv.channel : " + ctv.channel);
+//        ctv.displayCation("Hello, World");
+//        ctv.caption = true;
+//        ctv.displayCation("Hello, World");
+//    }
 }
 
-class Tv {
-    boolean power;
-    int channel;
-    void power() { power = !power; }
-    void channelUp() { ++channel; }
-    void channelDown() { --channel; }
-}
+//class Tv {
+//    boolean power;
+//    int channel;
+//    void power() { power = !power; }
+//    void channelUp() { ++channel; }
+//    void channelDown() { --channel; }
+//}
 
 class CaptionTv extends Tv {
     boolean caption;
--- src/main/java/Study/hajju/ch7/SuperTest.java
+++ src/main/java/Study/hajju/ch7/SuperTest.java
@@ -1,18 +1,18 @@
 package Study.hajju.ch7;
 
 public class SuperTest {
-    public static void main(String[] args) {
-        Child c = new Child();
-        c.method();
-    }   //main
+//    public static void main(String[] args) {
+//        Child c = new Child();
+//        c.method();
+//    }
 }
-class Parent {
-    int x = 10;
-}
-class Child extends Parent {
-    void method() {
-        System.out.println("x = " + x);
-        System.out.println("this.x = " + this.x);
-        System.out.println("super.x = " + super.x);
-    }
-}
+//class Parent {
+//    int x = 10;
+//}
+//class Child extends Parent {
+//    void method() {
+//        System.out.println("x = " + x);
+//        System.out.println("this.x = " + x);
+//        System.out.println("super.x = " + x);
+//    }
+//}
