5a81c9bea1b367f499c93c823b0d40b959ddab76
hajju0617 <joowon023617@gmail.com>
2024-04-28 01:52:11 +0900

    first

--- src/main/java/Study/hajju/ch7/CastingTest1.java
+++ src/main/java/Study/hajju/ch7/CastingTest1.java
@@ -2,34 +2,34 @@ package Study.hajju.ch7;
 
 public class CastingTest1 {
     public static void main(String[] args) {
-        Car car = null;
-        FireEngine fe = new FireEngine();
-        FireEngine fe2 = null;
-
-        fe.water();
-        car = fe;
-//        car.water();
-        fe2 = (FireEngine) car;
-        fe2.water();
+//        Car car = null;
+//        FireEngine fe = new FireEngine();
+//        FireEngine fe2 = null;
+//
+//        fe.water();
+//        car = fe;
+////        car.water();
+//        fe2 = (FireEngine) car;
+//        fe2.water();
 
     }
 }
 
-class Car {
-    String color;
-    int door;
-
-    void drive() {
-        System.out.println("drive, Brrrrrr~");
-    }
-    void stop() {
-        System.out.println("Stop!!!");
-    }
-}
-
-class FireEngine extends Car {
-    void water() {
-        System.out.println("Water!!!");
-    }
-}
+//class Car {
+//    String color;
+//    int door;
+//
+//    void drive() {
+//        System.out.println("drive, Brrrrrr~");
+//    }
+//    void stop() {
+//        System.out.println("Stop!!!");
+//    }
+//}
+//
+//class FireEngine extends Car {
+//    void water() {
+//        System.out.println("Water!!!");
+//    }
+//}
 
